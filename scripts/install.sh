410e5664e64ad458b31811e6197707c8f1320e7a
khankhelasim <khankhelasim@gmail.com>
2025-06-18 19:10:02 +0500

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
+
+# Update system packages
 sudo apt update
+
+# Install pip and necessary tools
 sudo apt install -y python3-pip
-cd /home/ubuntu/flaskapp || exit
-pip3 install -r requirements.txt
+sudo apt install -y software-properties-common
+
+# Add deadsnakes PPA for Python 3.9
+sudo add-apt-repository --yes ppa:deadsnakes/ppa
+sudo apt update
+
+# Install Python 3.9
+sudo apt install -y python3.9
+
+# Check Python version
+python3.9 --version
+
+# Install distutils (required for installing pip via script)
+sudo apt install python3.9-distutils -y
+
+# Download and install pip for Python 3.9
+sudo wget https://bootstrap.pypa.io/get-pip.py
+sudo python3.9 get-pip.py
+
+# Check pip version
+pip3.9 --version
+
+# Install Flask
+pip3.9 install --ignore-installed Flask
